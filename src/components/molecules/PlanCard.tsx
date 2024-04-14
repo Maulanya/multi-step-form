48618d1584abf544337b912f136b0a1fd1e56c52
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2024-04-15 00:12:58 +0800

    fix: bug multiple select plan

--- src/components/molecules/PlanCard.tsx
+++ src/components/molecules/PlanCard.tsx
@@ -10,47 +10,21 @@ import { useContext } from "react"
 
 
 interface PlanCardProps {
-    children: string,
-    ActivePlan: boolean,
-    setActivePlan?: Function
+    plan: string
+    onClick?: () => void
+    ActivePlan: string
 }
 
-export const PlanCard = ({ children, ActivePlan, setActivePlan }: PlanCardProps) => {
-    const [Active, setActive] = useState<boolean>(false)
+export const PlanCard = (props: PlanCardProps) => {
     const SwitchPlan = useContext(Context)
-    // useEffect(() => {
-    //     if (Active) {
-    //         SwitchPlan.Plan.push(children);
-    //         // setActivePlan(SwitchPlan.Plan[0])
-    //         if (SwitchPlan.Plan.length > 1) {
-    //             SwitchPlan.Plan.shift()
-    //         }
-    //         console.log(SwitchPlan.Plan[0] === children, children);
-    //     } else {
-    //         console.log(SwitchPlan.Plan[0]);
-    //     }
-    // }, [Active])
-
-    const HandleActive = useCallback(() => {
-        setActive(true)
-        SwitchPlan.Plan.push(children);
-        // setActivePlan(SwitchPlan.Plan[0])
-        if (SwitchPlan.Plan.length > 1) {
-            SwitchPlan.Plan.shift()
-        }
-        console.log(SwitchPlan.Plan[0] === children, children);
-    }, [Active])
-
     return (
-        <div onClick={HandleActive} className={
-            clsx('cursor-pointer py-5 px-5 w-[180px] grid gap-10 border-solid border-[2px] border-[hsl(229_24%_87%)] rounded-md', {
-                '!border-[hsl(243_100%_62%)]': Active
-            })
-        } >
-            <img src={children === 'Arcade' ? Arcade : children === 'Advanced' ? Advanced : Pro} alt="" />
+        <div onClick={props.onClick}
+            className={`cursor-pointer py-5 px-5 w-[180px] grid gap-10 border-solid border-[2px] rounded-md ${props.ActivePlan === props.plan ? "border-[hsl(243_100%_62%)]" : "!border-[hsl(229_24%_87%)]"}`} 
+            >
+            <img src={props.plan === 'Arcade' ? Arcade : props.plan === 'Advanced' ? Advanced : Pro} alt="" />
             <div>
-                <PlanTitle>{children}</PlanTitle>
-                <PlanPrice price={children === 'Arcade' ? SwitchPlan.Switch ? '$90/yr' : '$9/mo' : children === 'Advanced' ? SwitchPlan.Switch ? '$120/yr' : '$12/mo' : SwitchPlan.Switch ? '$150/yr' : '$15/mo'} />
+                <PlanTitle>{props.plan}</PlanTitle>
+                <PlanPrice price={props.plan === 'Arcade' ? SwitchPlan.Switch ? '$90/yr' : '$9/mo' : props.plan === 'Advanced' ? SwitchPlan.Switch ? '$120/yr' : '$12/mo' : SwitchPlan.Switch ? '$150/yr' : '$15/mo'} />
                 {
                     SwitchPlan.Switch && <p className="text-[hsl(213_96%_18%)] text-sm font-UbuntuMedium mt-2">2 months free</p>
                 }
--- src/components/organism/SelectPlan.tsx
+++ src/components/organism/SelectPlan.tsx
@@ -5,15 +5,23 @@ import { useContext } from "react"
 import { Context } from "../../Context"
 export const SelectPlan = () => {
     const Plan = useContext(Context)
-    // const [ActivePlan, setActivePlan] = useState<string>('')
+    const [ActivePlan, setActivePlan] = useState<string>('')
+
+    const selectedPlan = (params: string) => {
+        if (params === ActivePlan) {
+            return setActivePlan("")
+        }
+        setActivePlan(params)
+    }
+
     return (
         <>
             <h3 className="font-UbuntuBold text-4xl">Select your plan</h3>
             <p className="text-[hsl(231_11%_63%)] text-xl">You have the option of monthly or yearly billing.</p>
             <div className="mt-10 flex gap-5">
-                <PlanCard ActivePlan={'Arcade' === Plan.Plan[0]}>Arcade</PlanCard>
-                <PlanCard ActivePlan={"Advanced" === Plan.Plan[0]}>Advanced</PlanCard>
-                <PlanCard ActivePlan={'Pro' === Plan.Plan[0]}>Pro</PlanCard>
+                <PlanCard plan="Arcade" onClick={() => selectedPlan("Arcade")} ActivePlan={ActivePlan}/>
+                <PlanCard plan="Adcaned" onClick={() => selectedPlan("Adcaned")} ActivePlan={ActivePlan}/>
+                <PlanCard plan="Pro" onClick={() => selectedPlan("Pro")} ActivePlan={ActivePlan}/>
             </div>
             <SwitchPlan />
         </>
